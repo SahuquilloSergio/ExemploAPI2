0c230bb398ae26c61a29598d2b7e8e0dceae4b86
serxa <sergiose2010@gmail.com>
2018-04-12 09:33:49 +0200

    metodo de commit añadido

--- src/main/java/com/sergio/pruebapi2/Metodos.java
+++ src/main/java/com/sergio/pruebapi2/Metodos.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.eclipse.jgit.api.AddCommand;
+import org.eclipse.jgit.api.CommitCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.Repository;
@@ -32,6 +34,27 @@ public class Metodos {
                     .build();
         } catch (IOException ex) {
             Logger.getLogger(Metodos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void commit(String ruta, String msn){
+        try{
+            FileRepositoryBuilder repositoryBuilder=new FileRepositoryBuilder();
+            Repository repository=repositoryBuilder.setGitDir(new File(ruta))
+                    .readEnvironment()
+                    .findGitDir()
+                    .setMustExist(true)
+                    .build();
+
+            Git git=new Git(repository);
+            AddCommand add=git.add();
+            add.addFilepattern(ruta).call();
+            CommitCommand commit=git.commit();
+            commit.setMessage(msn).call();
+        }catch(IOException ex){
+            System.out.println("Error:"+ex);
+        }catch(GitAPIException ex){
+            System.out.println("Error:"+ex);
         }
     }
 }
